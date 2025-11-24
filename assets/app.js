b05db39b8982a870a810847eb6998d9475f50c05
PrabhuDeva2001 <puliprabhudeva@gmail.com>
2025-11-24 10:31:52 +0530

    final update

--- assets/app.js
+++ assets/app.js
@@ -4,12 +4,7 @@ const CATEGORY_API = "https://www.themealdb.com/api/json/v1/1/categories.php";
 const SEARCH_API =  "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 const DETAILS_API = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 const FILTER_BY_CATEGORY_API =  "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
-// let data = [];
 
-// console.log(CATEGORY_API);
-// console.log(SEARCH_API + 'Beef')
-// console.log(FILTER_BY_CATEGORY_API+'chicken');
-// console.log(DETAILS_API+53099);
 
 async function products() {
   let pr = await fetch(CATEGORY_API);
@@ -68,48 +63,6 @@ async function menubar(){
   });
 }
 menubar();
-
-// seraching 
-
-// async function searchitem(item){
-//  let res= await fetch(SEARCH_API+item)
-//  let data = await res.json()
-//  return data
- 
-// }
-
-// // let input=document.getElementById('input').value.trim()
-// let btn=document.getElementById('searchbtn')
-// let searchitemdisplay=document.getElementById('searchitemdisplay')
-// btn.addEventListener('click',async ()=>{
-//   let input=document.getElementById('input').value.trim()
-//  let res= await  searchitem(input)
- 
-//  let ar=res.meals;
-//  searchitemdisplay.innerHTML='';
-//  ar.forEach((item)=>{
-//    let card = document.createElement('div');
-//     card.className = 'card col-12 col-md-6 col-lg-3 m-lg-3 p-2 text-center text-white';
-//     card.innerHTML = `
-//       <img src="${item.strMealThumb}" alt="${item.strMeal}" class="img1 pb-1 rounded">
-//       <p class="fw-light rounded px-2 py-1 fs-6 fs-md-5 fs-lg-4">${item.strMeal}</p>
-//     `;
-
-//     // card.addEventListener('click',()=>{
-//     //   detailsitem(item.idMeal)
-//     // })
-    
-//   searchitemdisplay.appendChild(card)
-//  })
- 
-// })
-// food details
-// async function detailsitem(item) {
-//   let res=await fetch(DETAILS_API+item)
-//   let data = await res.json()
-//   console.log(data);
-  
-// }
 
 async function searchitem(item) {
   let res = await fetch(SEARCH_API + item);
@@ -182,7 +135,7 @@ function displaymeals(meals) {
     `;
     cards.addEventListener('click', () => {
       Meald.innerHTML=' ';
-      detailsAboutpaticularitem(meal.idMeal); //paticular item will display with full information 
+      detailsAboutpaticularitem(meal.idMeal); 
     });
 
     searchitemdisplay.appendChild(cards);
